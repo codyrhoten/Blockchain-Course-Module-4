cea74c6062d7ad936f7363446c595433fecb2516
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-05-30 23:53:15 -0400

    start voting DApp

---
+++ 2-DApp-Architectures/voting-DApp/public/client.js
@@ -0,0 +1,261 @@
+$(document).ready(function () {
+    const ethereumProvider = ethers.providers.getDefaultProvider('ropsten');
+    const votingContractAddress = "0xE39d68C746Dc1ad5ed2EEbf9B8864d1BFd965631";
+    const votingContractABI = [
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "name",
+                    "type": "string"
+                }
+            ],
+            "name": "addCandidate",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "index",
+                    "type": "uint32"
+                }
+            ],
+            "name": "vote",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "candidatesCount",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint32"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "name": "index",
+                    "type": "uint32"
+                }
+            ],
+            "name": "getCandidate",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "name": "index",
+                    "type": "uint32"
+                }
+            ],
+            "name": "getVotes",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint32"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+    const votingContract = new ethers.Contract(
+        votingContractAddress, votingContractABI, ethereumProvider
+    );
+
+    showView("viewHome");
+
+    $('#linkHome').click(function () {
+        showView("viewHome");
+    });
+
+    $('#linkLogin').click(function () {
+        showView("viewLogin");
+    });
+
+    $('#linkRegister').click(function () {
+        showView("viewRegister");
+    });
+
+    $('#linkLogout').click(logout);
+
+    $('#buttonLogin').click(login);
+    $('#buttonRegister').click(register);
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+        const loggedIn = sessionStorage.jsonWallet;
+        if (loggedIn) {
+            $('.show-after-login').show();
+            $('.hide-after-login').hide();
+        } else {
+            $('.show-after-login').hide();
+            $('.hide-after-login').show();
+        }
+        if (viewName === 'viewHome')
+            loadVotingResults();
+    }
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function () { $("#ajaxLoadingBox").fadeIn(200) },
+        ajaxStop: function () { $("#ajaxLoadingBox").fadeOut(200) }
+    });
+
+    function showInfo(message) {
+        $('#infoBox>p').html(message);
+        $('#infoBox').show();
+        $('#infoBox>header').click(function () {
+            $('#infoBox').hide();
+        });
+    }
+
+    function showError(errorMsg, err) {
+        let msgDetails = "";
+        if (err && err.responseJSON)
+            msgDetails = err.responseJSON.errorMsg;
+        $('#errorBox>p').html('Error: ' + errorMsg + msgDetails);
+        $('#errorBox').show();
+        $('#errorBox>header').click(function () {
+            $('#errorBox').hide();
+        });
+    }
+
+    function showProgressBox(percent) {
+        let msg = "Wallet encryption / decryption ... " +
+            Math.round(percent * 100) + "% complete";
+        $('#progressBox').html(msg).show(100);
+    }
+
+    function hideProgressProgress() {
+        $('#progressBox').hide(100);
+    }
+
+    async function login() {
+        let username = $('#usernameLogin').val();
+        let walletPassword = $('#passwordLogin').val();
+        let backendPassword = CryptoJS
+            .HmacSHA256(username, walletPassword).toString();
+        try {
+            let result = await $.ajax ({
+                type: 'POST',
+                url: '/login',
+                data: JSON.stringify({
+                    username,
+                    password: backendPassword
+                }),
+                contentType: 'application/json'
+            });
+            sessionStorage['username'] = username;
+            sessionStorage['jsonWallet'] = result.jsonWallet;
+            showView('viewHome');
+            showInfo(`User "${username}" logged in successfully`);
+        } catch (err) {
+            showError('Cannot login user. ' + err);
+        }
+    }
+
+    async function register() {
+        let username = $('#usernameRegister').val();
+        let walletPassword = $('#passwordRegister').val();
+
+        try {
+            let wallet = ethers.Wallet.createRandom();
+            let jsonWallet = await wallet
+                .encrypt(walletPassword, {}, showProgressBox);
+            let backendPassword = CryptoJS
+                .HmacSHA256(username, walletPassword).toString();
+
+            let result = await $.ajax({
+                type: 'POST',
+                url: '/register',
+                data: JSON.stringify({
+                    username,
+                    password: backendPassword,
+                    jsonWallet
+                }),
+                contentType: 'application/json'
+            });
+            sessionStorage['username'] = username;
+            sessionStorage['jsonWallet'] = jsonWallet;
+            showView('viewHome');
+            showInfo(`User ${username} registered successfully. Please save your mnemonics: <b>${wallet.mnemonic.phrase}</b>`);
+        } catch (err) {
+            showError('Cannot register user.', err);
+        } finally {
+            hideProgressProgress();
+        }
+    }
+
+    async function loadVotingResults() {
+        try {
+            // Load the candidates from the Ethereum smart contract
+            let candidates = [];
+            let candidatesCount = await votingContract.candidatesCount();
+
+            for (let i = 0; i < candidatesCount; i++) {
+                let candidate = await votingContract.getCandidate(i);
+                let votes = await votingContract.getVotes(i);
+                candidates.push({ candidate, votes });
+            }
+            // Display the candidates
+            let votingResultsUl = $('#votingResults').empty();
+
+            for (let i = 0; i < candidatesCount; i++) {
+                let candidate = candidates[i];
+                let li = $('<li>')
+                    .html(`${candidate.candidate} -> ${candidate.votes} votes `);
+
+                if (sessionStorage['username']) {
+                    let button = $(`<input type="button" value="Vote">`);
+                    button.click(function () {
+                        vote(i, candidate.candidate)
+                    });
+                    li.append(button);
+                }
+                
+                li.appendTo(votingResultsUl);
+            }
+        } catch (err) {
+            showError(err);
+        }
+    }
+
+    async function vote(candidateIndex, candidateName) {
+        //TODO:
+    }
+
+    function logout() {
+        sessionStorage.clear();
+        showView('viewHome');
+    }
+
+});
+
+
