bb730b84da257487a49e5601073a96bfad70fe40
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-09-03 11:17:59 -0400

    add demos to server-side Ethereum APIs

--- 5-Ethereum-Server-Side-Libraries/demos/web3.js/01_example.js
+++ 5-Ethereum-Server-Side-Libraries/demos/web3.js/01_example.js
@@ -1,5 +1,5 @@
 let Web3 = require('web3')
-let web3 = new Web3("http://localhost:8545")
+let web3 = new Web3("http://localhost:7545")
 
 web3.eth.getCoinbase().then(function(addr) {
     console.log(addr)
--- 5-Ethereum-Server-Side-Libraries/demos/web3.js/02_example.js
+++ 5-Ethereum-Server-Side-Libraries/demos/web3.js/02_example.js
@@ -1,8 +1,35 @@
 let Web3 = require('web3')
-let web3 = new Web3("http://localhost:8545")
+let web3 = new Web3("http://localhost:7545")
 
 const contractAddress = '0x6ce78f7700ac45c07a9bcf4619c7d39c637f5b60';
-const contractABI = [{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
+const contractABI = [
+	{
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
 let contract = new web3.eth.Contract(contractABI, contractAddress);
 
 contract.methods.get()
