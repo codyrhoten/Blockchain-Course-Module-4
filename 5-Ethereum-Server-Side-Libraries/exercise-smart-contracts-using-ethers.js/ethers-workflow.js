db9099a59e3a142476beb80d0a172bec8557dca3
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-09-07 21:29:57 -0400

    start ethers.js smart contracts exercise

---
+++ 5-Ethereum-Server-Side-Libraries/exercise-smart-contracts-using-ethers.js/ethers-workflow.js
@@ -0,0 +1,6 @@
+const ethers = require('ethers');
+const solc = require('solc');
+const Contract = ethers.Contract;
+const provider = ethers.getDefaultProvider('ropsten');
+const fs = require('fs-extra');
+
