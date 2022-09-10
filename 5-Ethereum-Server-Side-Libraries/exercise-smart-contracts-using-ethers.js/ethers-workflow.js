23cf2d183be0369ac6e6a082abcb0a828e70235d
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-09-10 14:59:16 -0400

    create fn addFact

--- 5-Ethereum-Server-Side-Libraries/exercise-smart-contracts-using-ethers.js/ethers-workflow.js
+++ 5-Ethereum-Server-Side-Libraries/exercise-smart-contracts-using-ethers.js/ethers-workflow.js
@@ -30,21 +30,40 @@ function compileContract(fileName, contractName) {
     return output[contractName];
 }
 
-/* (async() => {
+const privateKey = '109ed597e869eb1f3cb5b0e4d23553f0187b51a7a8a4ae1108094e9ffc08b061';
+
+(async() => {
     console.log('\nCOMPILING CONTRACT');
     const compiledContract = compileContract('./ArrayOfFacts.sol', "ArrayOfFacts");
     console.log(compiledContract);
     const abi = compiledContract.abi;
-})(); */
 
-const privateKey = 'b33688aba6fcfcc17668c27686e5a70c0259f7d289980f82da91d76584f953dc';
+    /* console.log('\nDEPLOYING CONTRACT');
+    let contract = await deployContract(
+        privateKey, 
+        './ArrayOfFacts.sol', 
+        'ArrayOfFacts'
+    );
+    console.log('\nWAITING FOR CONTRACT TO BE MINED');
+    await contract.deployed();
+    console.log(contract.address); */
+    
+    let contractAddress = '0xE6919F8Ff6EDb9A1f165EB44904bfb04d2B591c3'
+
+    console.log('\nADDING A FACT');
+    let fact = 'The Times 03/Jan/2009 Chancellor on brink of second bailout for the banks!';
+    let tx = await addFact(privateKey, abi, contractAddress, fact);
+    console.log('\nWAITING FOR TRANSACTION TO BE MINED');
+    await tx.wait();
+})();
+
 
 function deployContract(privateKey, fileName, contractName) {
     let wallet = new ethers.Wallet(privateKey, provider);
     let contract = compileContract(fileName, contractName);
     let bytecode = '0x' + contract.evm.bytecode.object;
     let abi = contract.abi;
-    let factory = new Ethers.ContractFactory(abi, bytecode, wallet);
+    let factory = new ethers.ContractFactory(abi, bytecode, wallet);
 
     return factory.deploy().then(contract => {
         console.log('Transaction created: ');
@@ -52,4 +71,14 @@ function deployContract(privateKey, fileName, contractName) {
         console.log('Contract address: ' + contract.address);
         return contract;
     });
+
+    function addFact(privateKey, abi, contractAddress, fact) {
+        let wallet = new ethers.Wallet(privateKey, provider);
+        let contract = new ethers.Contract(contractAddress, abi, wallet);
+
+        return contract.add(fact).then(tx => {
+            console.log('Transaction: \n' + tx);
+            return tx;
+        })
+    }
 }
